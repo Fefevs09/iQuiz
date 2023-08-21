871a1971ff1200bdc97ec80df232937e2fdf11c4
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-08-21 11:40:27 -0300

    add ViewController

---
+++ iQuiz/QuestaoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuestaoViewController.swift
+//  iQuiz
+//
+//  Created by Felipe Vieira on 21/08/23.
+//
+
+import UIKit
+
+class QuestaoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
