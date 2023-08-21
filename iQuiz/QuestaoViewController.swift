bf14d447b8c1cbaf939823c1c1c1dea173551f02
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-08-21 16:12:50 -0300

    add dynamic title question

--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class QuestaoViewController: UIViewController {
+    
+    var pontuacao = 0
+    var numeroQuestao = 0
+    
 
     @IBOutlet weak var tituloQuestaoLabel: UILabel!
     
@@ -16,18 +20,24 @@ class QuestaoViewController: UIViewController {
     @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
         
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configurarLayout()
+        configurarQuestao()
     }
     
     func configurarLayout() {
         navigationItem.hidesBackButton = true
-        
+        tituloQuestaoLabel.numberOfLines = 0
+        tituloQuestaoLabel.textAlignment = .center
         for button in botoesRespostas {
             button.layer.cornerRadius = 12.0
         }
+    }
+    
+    func configurarQuestao() {
+        tituloQuestaoLabel.text = questoes[numeroQuestao].titulo
     }
 
 }
