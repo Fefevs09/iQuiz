6c41a76530569bad8a5c9b6f816aa95d46ce5b2e
Felipe Vieira <felipefernandes.contato09@gmail.com>
2023-08-22 00:59:15 -0300

    add write and wrong answer color

--- iQuiz/QuestaoViewController.swift
+++ iQuiz/QuestaoViewController.swift
@@ -9,16 +9,29 @@ import UIKit
 
 class QuestaoViewController: UIViewController {
     
+    //   MARK: Variables
     var pontuacao = 0
     var numeroQuestao = 0
-    
 
     @IBOutlet weak var tituloQuestaoLabel: UILabel!
     
     @IBOutlet var botoesRespostas: [UIButton]!
     
     @IBAction func respostaBotaoPressionado(_ sender: UIButton) {
+        let respostaCerta = questoes[numeroQuestao].respostaCorreta == sender.tag
         
+        if(respostaCerta) {
+            pontuacao += 1
+            sender.backgroundColor = UIColor(red: 11/255, green: 161/255, blue: 53/255, alpha: 1.0)
+            
+        } else {
+            sender.backgroundColor = UIColor(red: 211/255, green: 17/255, blue: 17/255, alpha: 1.0)
+        }
+        
+        if numeroQuestao < questoes.count - 1 {
+            numeroQuestao += 1
+            Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(configurarQuestao), userInfo: nil, repeats: false)
+        }
     }
     
     override func viewDidLoad() {
@@ -31,13 +44,20 @@ class QuestaoViewController: UIViewController {
         navigationItem.hidesBackButton = true
         tituloQuestaoLabel.numberOfLines = 0
         tituloQuestaoLabel.textAlignment = .center
+        
         for button in botoesRespostas {
             button.layer.cornerRadius = 12.0
         }
     }
     
-    func configurarQuestao() {
+    @objc func configurarQuestao() {
         tituloQuestaoLabel.text = questoes[numeroQuestao].titulo
+        
+        for botao in botoesRespostas {
+            let tituloBotao = questoes[numeroQuestao].respostas[botao.tag]
+            botao.setTitle(tituloBotao, for: .normal)
+            botao.backgroundColor = UIColor(red: 116/255, green: 50/255, blue: 255/255, alpha: 1.0)
+        }
     }
 
 }
